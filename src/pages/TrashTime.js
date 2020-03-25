0a502a1d3f17cfeffb9ab4eedf6ed5ebdce31e53
Weslley de Campos <weslleydrum@hotmail.com>
2020-03-25 12:43:29 -0300

    fazendo requisições para abrir os horários dos lix

--- src/pages/TrashTime.js
+++ src/pages/TrashTime.js
@@ -1,21 +1,82 @@
 import React, { useState, useEffect } from 'react';
-import { View, StyleSheet, TouchableOpacity, Text } from 'react-native';
+import { View, StyleSheet, Alert, TouchableOpacity, Text, Picker, AsyncStorage } from 'react-native';
 import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-table-component';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
-export default function TrashTime({navigation}) {
-    const handleViewOcurrances = () =>{
+import api from '../services/api';
+export default function TrashTime({ navigation }) {
+    const [city, setCity] = useState('')
+    const [options, setOptions] = useState([])
+
+    const handleViewOcurrances = () => {
         navigation.navigate('ViewOcurrance')
+    }
+
+    useEffect(() => {
+        async function getCities() {
+            try {
+                let result = await api.get('/city',
+                    {
+                        headers: {
+                            Authorization: `Bearer ${await AsyncStorage.getItem('token')}`
+                        }
+                    }
+                )
+                setOptions(JSON.parse(result.request._response));
+            } catch (e) {
+                Alert.alert(
+                    'ATENÇÃO',
+                    'Ocorreu um erro ao pegar os dados !!'
+                )
+            }
+
+        }
+        getCities();
+    }, [])
+
+    const loadTrashTruckTimeIn = async () => {
+        let result = await api.get(`/loadtrashtimein/${city}`, {
+            headers: {
+                Authorization: `Bearer ${await AsyncStorage.getItem('token')}`
+            }
+        })
+    }
+    function loadServices() {
+        let serviceItems = options.map((option, index) => {
+
+            return <Picker.Item label={option.cityName} value={option._id} key={index} />
+        })
+        return serviceItems;
+    }
+    let valueChange = async (itemValue, itemIndex) => {
+        setCity(itemValue)
+        
+        let result = await api.get(`/trashtimein/${itemValue}`,
+            {
+                headers: {
+                    Authorization : `Bearer ${await AsyncStorage.getItem('token')}`
+                }
+            }
+        )
+        console.log(JSON.parse(result.request._response));
     }
     return (
         <View style={styles.container}>
-             <TouchableOpacity onPress={handleViewOcurrances} style={styles.button}>
+            <TouchableOpacity onPress={handleViewOcurrances} style={styles.button}>
                 <Icon name='arrow-left' size={25} color={'white'} />
             </TouchableOpacity>
-                
-                <Text style={styles.header}>Horários da coleta do lixo</Text>
 
-                <Table></Table>
-            
+            <Text style={styles.header}>Horários da coleta do lixo</Text>
+
+            <Text style={styles.label}>Escolha sua cidade</Text>
+            <Picker
+                selectedValue={city}
+                onValueChange={
+                    valueChange
+                }>
+                {
+                    loadServices()
+                }
+            </Picker>
         </View>
     )
 }
@@ -41,5 +102,9 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         margin: 10,
         fontWeight: 'bold'
+    },
+    label: {
+        textAlign: "center",
+        fontSize: 15
     }
 })
